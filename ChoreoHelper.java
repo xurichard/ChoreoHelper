be2eb351cf0c870375e186ddcbae755326943189
xurichard <richardxu@berkeley.edu>
2018-02-13 22:30:11 -0800

    First Commit

---
+++ ChoreoHelper.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ChoreoHelper
+{
+	
+	public ChoreoHelper(String start, String end){
+		// takes in start and end to create
+	}
+	
+	public static void main(String[] args)
+	{
+
+	}
+	
+}
+		
+		
